6007f4acd7feecbe1e805dd8052b945fddd8a883
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-07-05 23:35:44 +0530

    05-07-2025

---
+++ JULY-2025/05-07-2025/Max Score from Subarray Mins.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int pairWithMaxSum(vector<int> &arr) {
+        int maxSum = 0;
+        
+        for (int i = 0; i < arr.size() - 1; i++) {
+            maxSum = max(maxSum, arr[i] + arr[i + 1]);
+        }
+        
+        return maxSum;
+    }
+};
