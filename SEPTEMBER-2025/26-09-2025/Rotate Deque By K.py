0c273ba43caaab13566934f602a05b4c60670d51
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-09-26 22:28:06 +0530

    26-09-2025

---
+++ SEPTEMBER-2025/26-09-2025/Rotate Deque By K.py
@@ -0,0 +1,12 @@
+class Solution:    
+    def rotateDeque(self, dq, type, k):
+        n=len(dq)
+        k=(k%n)
+        if type==1:
+            while(k):
+                dq.appendleft(dq.pop())
+                k-=1
+        else:
+            while(k):
+                dq.append(dq.popleft())
+                k-=1
