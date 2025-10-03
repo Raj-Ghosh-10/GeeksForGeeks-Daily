02d45278cba40baddb85c9f27326906e33afb7f2
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-10-03 16:25:07 +0530

    03-10-2025

--- OCTOBER-2025/03-10-2025/Possible Words From Phone Digits.java
+++ OCTOBER-2025/03-10-2025/Possible Words From Phone Digits.java
@@ -1,33 +1,58 @@
 
+
+//User function Template for Java
+
 class Solution {
-  public:
-  void possibleWordsRec(vector<int> &arr, int index, string &prefix, 
-                      vector<string> &padMap, vector<string> &res) {
-    if (index == arr.size()) {
-        res.push_back(prefix);
-        return;
-    }
-    int digit = arr[index];
-    if (digit < 2 || digit > 9) {
-        possibleWordsRec(arr, index + 1, prefix, padMap, res);
-        return;
+    // Function to find list of all words possible by pressing given numbers.
+    static ArrayList<String> possibleWords(int a[], int N) {
+        // Map for keypad mappings
+        Map<Integer, List<Character>> keypad = new HashMap<>();
+        
+        keypad.put(2, Arrays.asList('a', 'b', 'c'));
+        keypad.put(3, Arrays.asList('d', 'e', 'f'));
+        keypad.put(4, Arrays.asList('g', 'h', 'i'));
+        keypad.put(5, Arrays.asList('j', 'k', 'l'));
+        keypad.put(6, Arrays.asList('m', 'n', 'o'));
+        keypad.put(7, Arrays.asList('p', 'q', 'r', 's'));
+        keypad.put(8, Arrays.asList('t', 'u', 'v'));
+        keypad.put(9, Arrays.asList('w', 'x', 'y', 'z'));
+
+        // List to store all possible words
+        ArrayList<String> result = new ArrayList<>();
+        
+        // Recursive function to generate words
+        getSS(a, 0, N, result, keypad, "");
+        
+        return result;
     }
 
-    for (char ch : padMap[digit]) {
-        prefix.push_back(ch);
-        possibleWordsRec(arr, index + 1, prefix, padMap, res);
-        prefix.pop_back();
+    // Static recursive helper function to generate the combinations
+    public static void getSS(int a[], int index, int N, ArrayList<String> result, 
+                             Map<Integer, List<Character>> keypad, String curr) {
+        // Base case: if we've processed all digits, add the current word to the result
+        if (index == N) {
+            if (!curr.isEmpty())
+                result.add(curr);
+            return;
+        }
+        
+        // Handle special case for '1' and '0'
+        if (a[index] == 1) {
+            // Skip digit '1' (since it has no corresponding letters)
+            getSS(a, index + 1, N, result, keypad, curr);
+        } else if (a[index] == 0) {
+            // Handle digit '0' (usually mapped to space or nothing)
+            getSS(a, index + 1, N, result, keypad, curr + ' ');
+        } else {
+            // Handle regular digits (2-9)
+            int number = a[index];
+            List<Character> valuesForNum = keypad.get(number);
+            
+            // Loop over all characters mapped to the current digit
+            for (char value : valuesForNum) {
+                // Recursively build the word
+                getSS(a, index + 1, N, result, keypad, curr + value);
+            }
+        }
     }
 }
-
-    vector<string> possibleWords(vector<int> &arr) {
-        // code here
-        vector<string> res;
-    vector<string> padMap = {"", "", "abc", "def", "ghi", "jkl",
-                                     "mno", "pqrs", "tuv", "wxyz"};
-    string prefix = "";
-
-    possibleWordsRec(arr, 0, prefix, padMap, res);
-    return res;
-    }
-};
