fca4f06931d2dd17fa470f37419d06750406607e
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-10-09 19:54:15 +0530

    09-10-2025

---
+++ OCTOBER-2025/09-10-2025/Postorder Traversal.java
@@ -0,0 +1,26 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+    Node(int val){
+        data = val;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    public ArrayList<Integer> postOrder(Node root) {
+        // code here
+        ArrayList<Integer> res = new ArrayList<>();
+       traverse(res,root);
+       return res;
+    }
+    public static void traverse(ArrayList<Integer> res,Node root){
+        if(root == null) return;
+        
+        traverse(res,root.left);
+        traverse(res,root.right);
+        res.add(root.data);
+    }
+}
