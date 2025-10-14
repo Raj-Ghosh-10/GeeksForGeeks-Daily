d104567aa7f5b92d2ed0571e83bd6df1400fa2da
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-10-14 13:28:59 +0530

    14-10-2025

--- potd.java
+++ potd.java
@@ -1,26 +1,17 @@
-/*
-class Node {
-    int data;
-    Node left, right;
-
-    Node(int data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
-    }
-}
-*/
-class Solution{
-    private int[] solve(Node root){
-        if (root == null) return new int[]{0, 0};
-        int[] left = solve(root.left);
-        int[] right = solve(root.right);
-        int include = root.data + left[1] + right[1];
-        int exclude = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
-        return new int[]{include, exclude};
-    }
-    public int getMaxSum(Node root){
-        int[] raj = solve(root);
-        return Math.max(raj[0], raj[1]);
+class Solution {
+    public int nodeSum(Node root, int l, int r) {
+        if (root == null) return 0;
+        
+        int raj = 0;
+        if (root.data >= l && root.data <= r) {
+            raj += root.data;
+        }
+        if (root.data > l) {
+            raj += nodeSum(root.left, l, r);
+        }
+        if (root.data < r) {
+            raj += nodeSum(root.right, l, r);
+        }
+        return raj;
     }
 }
