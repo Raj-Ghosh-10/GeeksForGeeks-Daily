a02b15f2df3975cd8839fc36a41c312bd29b5876
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-10-15 19:19:40 +0530

    15-10-2025

--- potd.java
+++ potd.java
@@ -1,17 +1,37 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int d)
+    {
+        data = d;
+        left = right = null;
+    }
+}
+*/
+
 class Solution {
-    public int nodeSum(Node root, int l, int r) {
-        if (root == null) return 0;
+    int ans=-1;
+    int kk;
+    public int kthSmallest(Node root, int k) {
+        kk=k;
+        kth(root);
+        return ans;
+    }
+    public void kth(Node root) {
+        if(root.left==null && root.right==null){
+            kk--;
+            
+            if(kk==0){
+                ans=root.data;
+            }
+            return;
+        }
+        if(root.left!=null) kth(root.left);
+        kk--;
+        if(kk==0) ans=root.data;
+        if(root.right!=null) kth(root.right);
         
-        int raj = 0;
-        if (root.data >= l && root.data <= r) {
-            raj += root.data;
-        }
-        if (root.data > l) {
-            raj += nodeSum(root.left, l, r);
-        }
-        if (root.data < r) {
-            raj += nodeSum(root.right, l, r);
-        }
-        return raj;
     }
 }
