9c5f2a41e2e2ed9dcb6e85647c2638901b54234b
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-10-19 22:43:22 +0530

    19-10-2025

--- potd.java
+++ potd.java
@@ -13,20 +13,54 @@ class Node {
 */
 
 class Solution {
-    private void inorder(Node root,List<Integer> list){
-        if(root==null)return;
-        
-        inorder(root.left,list);
-        list.add(root.data);
-        inorder(root.right,list);
+    ArrayList<Integer> InoL = new ArrayList<>();
+
+    public void store(Node root) {
+        if (root == null) return;
+        store(root.left);
+        InoL.add(root.data);
+        store(root.right);
     }
-    public int findMedian(Node root) {
-        // Code here
-        List<Integer> list=new ArrayList<>();
-        inorder(root,list);
+
+    public int lowerB(List<Integer> inor, int target) {
+        int low = 0, high = inor.size();
+      
+        while (low <high) {
+            int mid = low + (high - low) / 2;
+            if (inor.get(mid) < target){
+                 low = mid + 1;
         
-        int n=list.size();
-        
-        return n%2==0 ? list.get((n/2)-1) : list.get(((n+1)/2)-1); // doing -1 because of 0 based indexing
+            }
+               
+            else if(inor.get(mid)==target) return mid;
+            
+            else
+                high = mid;
+        }
+        return low;
+    }
+
+    public ArrayList<Integer> getKClosest(Node root, int target, int k) {
+        store(root);
+        int idx = lowerB(InoL, target);
+        int left = idx - 1;
+        int right = idx;
+
+        ArrayList<Integer> ans = new ArrayList<>(); // <-- fix: declare ans
+
+        while (k-- > 0) {
+            if (left < 0) {
+                ans.add(InoL.get(right++));
+            } else if (right >= InoL.size()) {
+                ans.add(InoL.get(left--));
+            } else {
+                if (Math.abs(InoL.get(left) - target) <= Math.abs(InoL.get(right) - target))
+                    ans.add(InoL.get(left--));
+                else
+                    ans.add(InoL.get(right++));
+            }
+        }
+
+        return ans;
     }
 }
