6d18c2a98a18a9429864e134b2697cbea4bf059b
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-10-20 16:15:14 +0530

    20-10-2025

--- potd.java
+++ potd.java
@@ -1,66 +1,46 @@
-/*
-class Node {
-    int data;
-    Node left;
-    Node right;
-
-    Node(int data) {
-        this.data = data;
-        left = null;
-        right = null;
-    }
-}
-*/
-
 class Solution {
-    ArrayList<Integer> InoL = new ArrayList<>();
-
-    public void store(Node root) {
-        if (root == null) return;
-        store(root.left);
-        InoL.add(root.data);
-        store(root.right);
-    }
-
-    public int lowerB(List<Integer> inor, int target) {
-        int low = 0, high = inor.size();
-      
-        while (low <high) {
-            int mid = low + (high - low) / 2;
-            if (inor.get(mid) < target){
-                 low = mid + 1;
+    // Function to compute catalan number upto n
+    public void findCatalan(int n, int[] dp) {
+        if (n == 0) { // edge case
+            dp[0] = 1;
+            return;
+        }
         
+        // DP using Tabulation
+        
+        // Base cse : catalan of 0 and 1 = 1
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i=2; i<n+1; i++){
+            for(int j=0; j<i; j++){
+                // C(i) = Σ (C(j) * C(i-j-1)) 
+                dp[i] += dp[j] * dp[i-j-1];
             }
-               
-            else if(inor.get(mid)==target) return mid;
-            
-            else
-                high = mid;
         }
-        return low;
     }
-
-    public ArrayList<Integer> getKClosest(Node root, int target, int k) {
-        store(root);
-        int idx = lowerB(InoL, target);
-        int left = idx - 1;
-        int right = idx;
-
-        ArrayList<Integer> ans = new ArrayList<>(); // <-- fix: declare ans
-
-        while (k-- > 0) {
-            if (left < 0) {
-                ans.add(InoL.get(right++));
-            } else if (right >= InoL.size()) {
-                ans.add(InoL.get(left--));
-            } else {
-                if (Math.abs(InoL.get(left) - target) <= Math.abs(InoL.get(right) - target))
-                    ans.add(InoL.get(left--));
-                else
-                    ans.add(InoL.get(right++));
+    
+    public ArrayList<Integer> countBSTs(int[] arr) {
+        ArrayList<Integer> ans = new ArrayList<>(); // to store ans
+        
+        int n = arr.length;
+        
+        // dp[i] will store the ith Catalan number
+        int[] dp = new int[n+1]; // for Tabulation
+        findCatalan(n-1, dp); // precompute catalan number for each nodes
+        
+        
+        // Number of bst's when arr[i] is the root will be:
+        // Number of nodes to go in left subtree * Number of nodes to go in right subtree
+        for (int i = 0; i < n; i++) { // i = curr root
+            int left = 0; // number of elements to go in left subtree
+            int right = 0; // number of elements to go in right subtree
+            for (int j = 0; j < n; j++) {
+                if (arr[j] < arr[i]) left++; // count elements smaller than arr[i]
+                else if (arr[j] > arr[i]) right++; // count elements greater than arr[i]
             }
+            ans.add(dp[left] * dp[right]); // add possible BST's for each elements in ans
         }
-
         return ans;
     }
 }
