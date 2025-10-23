1d401ac3ebe3497b645af9a70ee0fb96ad9daef9
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-10-23 22:09:30 +0530

    23-10-2025

--- potd.java
+++ potd.java
@@ -1,23 +1,21 @@
 class Solution {
-    public void nearlySorted(int[] arr, int k) {
+    public ArrayList<ArrayList<Integer>> kClosest(int[][] points, int k) {
         // code here
-         PriorityQueue<Integer> pq = new PriorityQueue<>(); // min heap
+        int n = points.length;
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
         
-        for(int i=0; i<=k; i++){
-            // insert k elements from starting into pq
-            pq.offer(arr[i]);
+        Arrays.sort(points, java.util.Comparator.comparingInt(o -> o[0]*o[0] + o[1]*o[1]));
+        int idx = 0;
+        
+        while(k --> 0){
+            ArrayList<Integer> list = new ArrayList<>();
+            
+            list.add(points[idx][0]);
+            list.add(points[idx++][1]);
+            
+            ans.add(list);
         }
         
-        int index = 0; // for filling up the arr in sorted order
-        for(int i=k+1; i<arr.length; i++){
-            arr[index++] = pq.poll(); // extract min and store in arr
-            pq.add(arr[i]); // also store the next element in pq for further processing
-        }
-        
-        while(!pq.isEmpty()){
-            // store the remaining elements to arr
-            arr[index++] = pq.poll();
-        }
-    
+        return ans;
     }
 }
