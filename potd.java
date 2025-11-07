8bf4c6e6e581dc5c6ac84d4132a46a60f69c7726
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-11-07 06:43:48 +0530

    07-11-2025

--- potd.java
+++ potd.java
@@ -1,19 +1,30 @@
 class Solution {
-    public int numberOfWays(int n) {
-        if (n == 1) {
-            return 1;
+    public int maxProfit(int[][] jobs) {
+        Arrays.sort(jobs, (a, b) -> Integer.compare(a[1], b[1]));
+        int n = jobs.length;
+        int[] dp = new int[n];
+        dp[0] = jobs[0][2];
+        int[] endTimes = new int[n];
+        for (int i = 0; i < n; i++) endTimes[i] = jobs[i][1];
+        for (int i = 1; i < n; i++) {
+            int includeProfit = jobs[i][2];
+            int idx = binarySearch(endTimes, jobs[i][0], i - 1);
+            if (idx != -1) includeProfit += dp[idx];
+            dp[i] = Math.max(dp[i - 1], includeProfit);
         }
-        if (n == 2) {
-            return 2;
+        return dp[n - 1];
+    }
+    private int binarySearch(int[] endTimes, int startTime, int high) {
+        int low = 0, ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (endTimes[mid] <= startTime) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
-        int prev2 = 1;
-        int prev1 = 2;
-        int curr = 0;
-        for (int i = 3; i <= n; i++) {
-            curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
-        }
-        return prev1;
+        return ans;
     }
 }
