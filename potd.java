c240401010885c36ff59101e547630b311dd2394
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-11-13 09:45:18 +0530

    13-11-2025

--- potd.java
+++ potd.java
@@ -1,26 +1,19 @@
 class Solution {
-    public boolean wildCard(String txt, String pat) {
-        int n = pat.length();
-        int m = txt.length();
-        boolean[] prev = new boolean[m + 1];
-        boolean[] curr = new boolean[m + 1];
-        prev[0] = true;
-        for (int i = 1; i <= n; i++) {
-            curr[0] = prev[0] && pat.charAt(i - 1) == '*';
-            for (int j = 1; j <= m; j++) {
-                char p = pat.charAt(i - 1);
-                char t = txt.charAt(j - 1);
-                if (p == t || p == '?') {
-                    curr[j] = prev[j - 1];
-                } else if (p == '*') {
-                    curr[j] = prev[j] || curr[j - 1];
-                } else {
-                    curr[j] = false;
-                }
-            }
-            prev = curr.clone();
-            curr = new boolean[m + 1];
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int n = s1.length(), m = s2.length();
+        if (s3.length() != n + m) return false;
+        boolean[] dp = new boolean[m + 1];
+        dp[0] = true;
+        for (int j = 1; j <= m; j++) {
+            dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
         }
-        return prev[m];
+        for (int i = 1; i <= n; i++) {
+            dp[0] = dp[0] && s1.charAt(i - 1) == s3.charAt(i - 1);
+            for (int j = 1; j <= m; j++) {
+                dp[j] = (dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) ||
+                        (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+            }
+        }
+        return dp[m];
     }
 }
