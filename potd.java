4fcd9ab7090ffda15bb4dd668d555b33a00bc382
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-11-15 06:36:18 +0530

    15-11-2025

--- potd.java
+++ potd.java
@@ -1,31 +1,22 @@
 class Solution {
-    public static int mergeStones(int[] stones, int k) {
-        int n = stones.length;
-        if ((n - 1) % (k - 1) != 0) return -1;
-        int[] pref = new int[n + 1];
-        for (int i = 0; i < n; i++) pref[i + 1] = pref[i] + stones[i];
-        int[][][] dp = new int[n][n][k + 1];
-        int INF = (int)1e9;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++)
-                for (int t = 1; t <= k; t++)
-                    dp[i][j][t] = INF;
-            dp[i][i][1] = 0;
-        }
-        for (int len = 2; len <= n; len++) {
-            for (int i = 0; i + len - 1 < n; i++) {
-                int j = i + len - 1;
-                for (int t = 2; t <= k; t++) {
-                    for (int mid = i; mid < j; mid += (k - 1)) {
-                        if (dp[i][mid][1] == INF || dp[mid + 1][j][t - 1] == INF) continue;
-                        dp[i][j][t] = Math.min(dp[i][j][t],
-                                dp[i][mid][1] + dp[mid + 1][j][t - 1]);
-                    }
+    public int minCutCost(int n, int[] cuts) {
+        int m = cuts.length;
+        int[] arr = new int[m + 2];
+        for (int i = 0; i < m; i++) arr[i + 1] = cuts[i];
+        arr[0] = 0;
+        arr[m + 1] = n;
+        java.util.Arrays.sort(arr);
+        int[][] dp = new int[m + 2][m + 2];
+        for (int len = 2; len < m + 2; len++) {
+            for (int i = 0; i + len < m + 2; i++) {
+                int j = i + len;
+                int best = Integer.MAX_VALUE;
+                for (int k = i + 1; k < j; k++) {
+                    best = Math.min(best, dp[i][k] + dp[k][j] + arr[j] - arr[i]);
                 }
-                if (dp[i][j][k] < INF)
-                    dp[i][j][1] = dp[i][j][k] + (pref[j + 1] - pref[i]);
+                dp[i][j] = best == Integer.MAX_VALUE ? 0 : best;
             }
         }
-        return dp[0][n - 1][1];
+        return dp[0][m + 1];
     }
 }
