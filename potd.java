89a6e5827e04f52b33e4b83d81bcc52f2de12ebf
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-11-17 06:57:24 +0530

    17-11-2025

--- potd.java
+++ potd.java
@@ -1,20 +1,19 @@
 class Solution {
-    public int LCIS(int[] a, int[] b) {
-        int n = a.length, m = b.length;
-        int[] dp = new int[m];
+    public int maxSumIS(int arr[]) {
+        int n = arr.length;
+        int[] dp = new int[n];
         for (int i = 0; i < n; i++) {
-            int currentMax = 0;
-            for (int j = 0; j < m; j++) {
-                if (a[i] == b[j]) {
-                    dp[j] = currentMax + 1;
-                } 
-                else if (b[j] < a[i]) {
-                    currentMax = Math.max(currentMax, dp[j]);
+            dp[i] = arr[i];
+        }
+        int ans = arr[0];
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]);
                 }
             }
+            ans = Math.max(ans, dp[i]);
         }
-        int ans = 0;
-        for (int v : dp) ans = Math.max(ans, v);
         return ans;
     }
 }
