43de9760305c962cf68fca5cd016b62b98f635c6
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-11-22 10:49:06 +0530

    22-11-2025

--- potd.java
+++ potd.java
@@ -1,58 +1,36 @@
 class Solution {
-    static final long INF = (long)1e18;
+    int[] parent, rank;
 
-    long[] dijkstra(int V, int src, ArrayList<ArrayList<int[]>> adj) {
-        long[] dist = new long[V];
-        Arrays.fill(dist, INF);
-
-        PriorityQueue<long[]> pq = new PriorityQueue<>((x, y) -> Long.compare(x[0], y[0]));
-        dist[src] = 0;
-        pq.add(new long[]{0, src});
-
-        while (!pq.isEmpty()) {
-            long[] cur = pq.poll();
-            long d = cur[0];
-            int u = (int)cur[1];
-
-            if (d != dist[u]) continue;
-
-            for (int[] nxt : adj.get(u)) {
-                int v = nxt[0], w = nxt[1];
-                if (dist[v] > d + w) {
-                    dist[v] = d + w;
-                    pq.add(new long[]{dist[v], v});
-                }
-            }
-        }
-        return dist;
+    int find(int x) {
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
     }
-
-    public int shortestPath(int V, int a, int b, int[][] edges) {
-
-        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
-        for (int i = 0; i < V; i++) adj.add(new ArrayList<>());
-
-        for (int[] e : edges) {
-            int u = e[0], v = e[1], w1 = e[2];
-            adj.get(u).add(new int[]{v, w1});
-            adj.get(v).add(new int[]{u, w1});
+    void unite(int a, int b) {
+        int pa = find(a), pb = find(b);
+        if (pa == pb) return;
+        if (rank[pa] < rank[pb]) parent[pa] = pb;
+        else if (rank[pb] < rank[pa]) parent[pb] = pa;
+        else {
+            parent[pb] = pa;
+            rank[pa]++;
         }
-
-        long[] distA = dijkstra(V, a, adj);
-        long[] distB = dijkstra(V, b, adj);
-
-        long ans = distA[b];
-
+    }
+    public int minConnect(int V, int[][] edges) {
+        parent = new int[V];
+        rank = new int[V];
+        for (int i = 0; i < V; i++) parent[i] = i;
+        int redundant = 0;
         for (int[] e : edges) {
-            int u = e[0], v = e[1], w2 = e[3];
-
-            if (distA[u] < INF && distB[v] < INF)
-                ans = Math.min(ans, distA[u] + w2 + distB[v]);
-
-            if (distA[v] < INF && distB[u] < INF)
-                ans = Math.min(ans, distA[v] + w2 + distB[u]);
+            int u = e[0], v = e[1];
+            if (find(u) == find(v)) redundant++;
+            else unite(u, v);
         }
-
-        return ans >= INF ? -1 : (int)ans;
+        int components = 0;
+        for (int i = 0; i < V; i++) {
+            if (find(i) == i) components++;
+        }
+        int need = components - 1;
+        if (redundant >= need) return need;
+        return -1;
     }
 }
