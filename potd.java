9cbc90580a7b08243c2b6410644ac173fc48eae5
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-11-28 13:02:03 +0530

    28-11-2025

--- potd.java
+++ potd.java
@@ -1,18 +1,13 @@
 class Solution {
-    int subsetXORSum(int arr[]) {
-        int n = arr.length;
-        int ans = 0;
-
-        for (int bit = 0; bit <= 10; bit++) {
-            int countSet = 0;
-            for (int i = 0; i < n; i++) {
-                if ((arr[i] & (1 << bit)) != 0) {
-                    countSet++;
-                }
-            }
-            if (countSet > 0) {
-                ans += (1 << bit) * (1 << (n - 1));
-            }
+    public static ArrayList<Integer> subsetXOR(int n) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        int totalXOR = 0;
+        for (int i = 1; i <= n; i++) totalXOR ^= i;
+        if (totalXOR == n) {
+            for (int i = 1; i <= n; i++) ans.add(i);
+        } else {
+            int x = totalXOR ^ n;
+            for (int i = 1; i <= n; i++) if (i != x) ans.add(i);
         }
         return ans;
     }
